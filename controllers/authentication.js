102a869fc3e2e732d6af1ef4b3939aa3ad974adb
Timothy Craig <ttcraig@Timothys-MacBook-Pro.local>
2016-07-03 21:21:07 -0400

    add comments, fix typos, add cors

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -3,11 +3,13 @@ const User = require('../models/user');
 const config = require('../config');
 
 function tokenForUser(user) {
-  const timestamp = new Date().getTime()
+  const timestamp = new Date().getTime();
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
 exports.signin = function(req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need to give them a token
   res.send({ token: tokenForUser(req.user) });
 }
 
@@ -16,16 +18,19 @@ exports.signup = function(req, res, next) {
   const password = req.body.password;
 
   if (!email || !password) {
-    return res.status(422).send({ error: 'Please provide email & passowrd.' });
+    return res.status(422).send({ error: 'Please provide email and password'});
   }
 
-  User.findOne({ email: email, }, function(err, existingUser) {
+  // See if a user with the given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
     if (err) { return next(err); }
 
+    // If a user with email does exist, return an error
     if (existingUser) {
-      return res.status(422).send({ error: 'Email is already in use.' });
+      return res.status(422).send({ error: 'Email is already in use' });
     }
 
+    // If a user with email does NOT exist, create and save user record
     const user = new User({
       email: email,
       password: password
@@ -34,6 +39,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if (err) { return next(err); }
 
+      // Repond to request indicating the user was created
       res.json({ token: tokenForUser(user) });
     });
   });
--- router.js
+++ router.js
@@ -7,7 +7,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/', requireAuth, function(req, res) {
-    res.send({ hi: 'there' });
+    res.send({ message: 'authorized! hi there.' });
   });
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
