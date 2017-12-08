40ea4653ae4e179e53cb471b42e08275d11daed1
Timothy Craig <ttcraig@Timothys-MacBook-Pro.local>
2017-12-07 20:49:59 -0500

    es6 snytax cleanup

--- router.js
+++ router.js
@@ -1,14 +1,17 @@
-const Authentication = require('./controllers/authentication');
-const passportService = require('./services/passport');
-const passport = require('passport');
 
-const requireAuth = passport.authenticate('jwt', { session: false });
-const requireSignin = passport.authenticate('local', { session: false });
+const Authentication  = require('./controllers/authentication')
+const passportService = require('./services/passport')
+const passport        = require('passport')
 
-module.exports = function(app) {
-  app.get('/', requireAuth, function(req, res) {
-    res.send({ message: 'authorized! hi there.' });
-  });
-  app.post('/signin', requireSignin, Authentication.signin);
-  app.post('/signup', Authentication.signup);
+const requireAuth = passport.authenticate('jwt', { session: false })
+const requireSignin = passport.authenticate('local', { session: false })
+
+module.exports = app => {
+  app.get('/', requireAuth, (req, res) => {
+    res.send({ message: 'authorized! hi there.' })
+  })
+
+  app.post('/signin', requireSignin, Authentication.signin)
+
+  app.post('/signup', Authentication.signup)
 }
